943ad4058b85a31d0c05617643563e112f451e3e
Roman Solo <zlorient@gmail.com>
2019-03-26 09:47:17 +0300

    Create external JS

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function change_yellow() {
+		if (document.getElementById('yellow_button').onclick){
+        document.getElementById("main_img").src="Pics/KO.jpg";}
+		}
+function change_orange() {
+		if (document.getElementById('orange_button').onclick){
+        document.getElementById("main_img").src="Pics/MU.jpg";}
+		}
+function change_red() {
+		if (document.getElementById('red_button').onclick){
+        document.getElementById("main_img").src="Pics/KR.jpg";}
+		}
